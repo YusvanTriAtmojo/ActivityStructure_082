b764064944d6ceccf11aabec84b538bf0e24d6e5
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2023-04-05 11:26:11 +0700

    Menambahkan prosedur main dengan tipe int untuk memasukkan data ke dalam array dan menampilkan data sesuai dengan array.

--- Latihan3Structure/Latihan3Structure.cpp
+++ Latihan3Structure/Latihan3Structure.cpp
@@ -14,3 +14,31 @@ struct Mahasiswa
 	DetailAlamat alamat;
 	int umur;
 };
+
+int main() {
+	Mahasiswa mhs[50];
+
+	for (int i = 0; i < 50; i++) {
+		cout << "Masukan NIM : ";
+		cin.getline(mhs[i].nim, 12);
+		cout << "Masukan Umur : ";
+		cin >> mhs[i].umur;
+		cin.ignore(1, '\n');
+		cout << "Masukan Nama : ";
+		cin.getline(mhs[i].nama, 20);
+		cout << "Alamat : ";
+		cout << "\n\tMasukan Desa : ";
+		cin.getline(mhs[i].alamat.desa, 20);
+		cout << "\tMasukan Kota : ";
+		cin.getline(mhs[i].alamat.kota, 20);
+	}
+	
+	for (int i = 0; i < 50; i++) {
+		cout << "\n\nNIM : " << mhs[i].nim;
+		cout << "\nUmur : " << mhs[i].umur;
+		cout << "\nNama : " << mhs[i].nama;
+		cout << "\nAlamat : ";
+		cout << "\n\tDesa : " << mhs[i].alamat.desa;
+		cout << "\n\tKota : " << mhs[i].alamat.kota;
+	}
+}
